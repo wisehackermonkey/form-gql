bed6ab5bc0db5a3d70312acb08157f7f6ff656d4
Oran C <oranbusiness@gmail.com>
2019-04-27 00:26:17 -0700

    started #sumit() and added classes to inputs
    - added jquery
    - added classes to all the input fields
    - started submit() function
    - renamed conferm-pass -> retype

---
+++ public/assets/js/script.js
@@ -0,0 +1,21 @@
+/*
+by oran c
+github.com/wisehackermonkey
+oranbusiness@gmail.com
+20190426
+*/
+
+
+function submit() {
+    let username = $(".username-input").val()
+    let password = $(".password-input").val()
+    let retype_password = $(".retype-input").val()
+    let planet_name = $(".planet-name-input").val()
+
+
+    console.log(username)
+    console.log(password)
+    console.log(retype_password)
+    console.log(planet_name)
+    alert("__TODO__20190427_____")
+}
